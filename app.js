8037c6b1d9e5fe13e7e3a5aae4b0e88da6928774
jakea9 <jakea3201@gmail.com>
2024-07-17 00:20:42 +0100

    Challenge three completed.

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const getTopics = require('./controllers/topicController.js')
+const endpoints = require('./endpoints.json')
 
 const app = express()
 app.use(express.json())
+
+app.get('/api', (req, res, next) => {
+
+    res.status(200).send({endpoints: endpoints})
+
+})
 
 app.get('/api/topics', getTopics)
 
